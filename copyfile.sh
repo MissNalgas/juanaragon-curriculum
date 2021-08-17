338a6f602d5e6de6c8dcbd610edcb5cfab46ccbf
Miss Nalgas <sebasar997@gmail.com>
2021-08-16 19:20:10 -0500

    Added a the copy script

---
+++ copyfile.sh
@@ -0,0 +1,3 @@
+cp build/index.html ~/Dev/juanaragon.co/juanaragon-node/views/curriculum/
+cp -R build/* ~/Dev/juanaragon.co/juanaragon-node/static/curriculum/
+rm ~/Dev/juanaragon.co/juanaragon-node/static/curriculum/index.html
